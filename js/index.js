d8d670e69a08d9ba0cd411975eeb66efd4104bdb
Sergey <afkaimer@gmail.com>
2022-10-13 23:21:36 +0300

    almost finish fullscreen pages, need little fixed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 'use strict';
+
+//modal window
+function foo(){
+    let modalBtn = document.querySelector(".modal-btn");
+    let modalLayout = document.getElementById("modal-window");
+    let modalClose = document.querySelector(".modal-close");
+
+    let body = document.body;
+
+    modalBtn.addEventListener("click", () => {
+        modalLayout.classList.add("active");
+        body.classList.add("no-scroll");
+    });
+    modalClose.addEventListener("click", () => {
+        modalLayout.classList.remove("active");
+        body.classList.remove("no-scroll");
+    });
+
+    modalLayout.addEventListener("click", (e) => {
+        e.target.classList.remove("active");
+        body.classList.remove("no-scroll");
+    });
+
+}
+foo();
