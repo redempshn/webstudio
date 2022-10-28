9e8f176bcccc4e332a0226a340aa6aa78e76efd8
Sergey <afkaimer@gmail.com>
2022-10-28 18:29:06 +0300

    finish responsive

--- js/index.js
+++ js/index.js
@@ -2,21 +2,23 @@
 
 //modal window
 function foo(){
-    let modalBtn = document.querySelector(".modal-btn");
+    let modalBtn = document.querySelectorAll(".modal-btn");
     let modalLayout = document.getElementById("modal-window");
     let modalClose = document.querySelector(".modal-close");
 
     let body = document.body;
 
-    modalBtn.addEventListener("click", () => {
-        modalLayout.classList.add("active");
-        body.classList.add("no-scroll");
-    });
+    modalBtn.forEach(btn => {
+        btn.addEventListener("click", () => {
+            modalLayout.classList.add("active");
+            body.classList.add("no-scroll");
+        });
+    })
+
     modalClose.addEventListener("click", () => {
         modalLayout.classList.remove("active");
         body.classList.remove("no-scroll");
     });
-
     modalLayout.addEventListener("click", (e) => {
         e.target.classList.remove("active");
         body.classList.remove("no-scroll");
@@ -29,18 +31,10 @@ foo();
 // burger
 function toggleNavigation() {
     let burgerBtn = document.querySelector(".burger");
-
-    // let social = document.querySelector(".header-social");
-    // let contact = document.querySelector(".contacts");
-    // let nav = document.querySelector(".nav");
     let header = document.querySelector(".header");
 
     burgerBtn.addEventListener("click", () => {
         burgerBtn.classList.toggle("active");
-
-        // social.classList.toggle("active");
-        // contact.classList.toggle("active");
-        // nav.classList.toggle("active");
         header.classList.toggle("active");
     }); 
 }
